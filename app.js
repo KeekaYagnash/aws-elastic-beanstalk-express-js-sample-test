89fce27db8e8960caac46db308a6a7e2bfb66370
KeekaYagnash <yagnash.keeka@disraptor.co.za>
2023-12-08 11:27:56 +0200

    Trying a web app

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello Eskom!"));
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+// Handle the root URL
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+);
+
+// Start the server
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
